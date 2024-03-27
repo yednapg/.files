324764f92cc888e98af0dd6346b3a47d39678691
Gaurav Pandey <yednapg@gmail.com>
2024-03-27 18:03:26 +0530

    major update

--- nvim/lua/kickstart/plugins/barbar.lua
+++ nvim/lua/kickstart/plugins/barbar.lua
@@ -3,6 +3,17 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
 	},
-	init = function() end,
-	opts = {},
+	init = function()
+		vim.g.barbar_auto_setup = false
+	end,
+	opts = {
+		animation = true,
+		auto_hide = 1,
+		clickable = true,
+		separator = { left = "▎", right = "" },
+		modified = { button = "●" },
+		sidebar_filetypes = {
+			NvimTree = true,
+		},
+	},
 }
